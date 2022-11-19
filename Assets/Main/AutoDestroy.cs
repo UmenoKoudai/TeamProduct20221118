78e047aa5ee513319228a4eafa8517a39032395c
umenokoudai <umenokoudai0805@gmail.com>
2022-11-19 11:33:56 +0900

    Mainを作って必要なものを格納

--- Assets/Main/AutoDestroy.cs
+++ Assets/Main/AutoDestroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Des : MonoBehaviour
+public class AutoDestroy : MonoBehaviour
 {
     public void ObjectDestroy()
     {
