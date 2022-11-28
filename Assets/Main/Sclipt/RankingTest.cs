ec5b5a17062a008c135210e0bcc9f6e7feac7801
umenokoudai <umenokoudai0805@gmail.com>
2022-11-28 20:45:13 +0900

    あとちょっとでいけそうな気がする

--- Assets/Main/Sclipt/RankingTest.cs
+++ Assets/Main/Sclipt/RankingTest.cs
@@ -8,17 +8,68 @@ using System.Linq;
 
 public class RankingTest : MonoBehaviour
 {
+    public class RankingData
+    {
+        public Dictionary<string, int> rankingData;
+    }
     [SerializeField] InputField _inputField;
-    Dictionary<string, int> dic = new Dictionary<string, int>();
+    [SerializeField] Text _rankingText;
+    RankingData _data = new RankingData();
+    public static Dictionary<string, int> dic = new Dictionary<string, int>();
     int score;
+
+
+    private void Start()
+    {
+        _data = OnLoad();
+        if (_data != null)
+        {
+            dic = _data.rankingData;
+        }
+    }
+
+    private void Update()
+    {
+        //if (!dic.Any())
+        //{
+        //    _rankingText.text = string.Join("\n", dic);
+        //}
+    }
 
     public void ScoreSave()
     {
         dic.Add(_inputField.text, score);
         score++;
-        foreach (var a in dic)
+        _data.rankingData = dic;
+        OnSave(_data);
+    }
+    //スコアをJSON形式で保存
+    public void OnSave(RankingData data)
+    {
+        using (StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/savedata.json"))
         {
-            Debug.Log($"名前：{a.Key} スコア：{a.Value}");
+            string json = JsonUtility.ToJson(data);
+            writer.Write(json);
+            //writer.Flush();
+            writer.Close();
+        }
+    }
+    //スコアの呼び出し
+    public RankingData OnLoad()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(Application.persistentDataPath + "/savedata.json"))
+            {
+                string datastr = reader.ReadLine();
+                reader.Close();
+                return JsonUtility.FromJson<RankingData>(datastr); ;
+            }
+        }
+        catch
+        {
+            Debug.LogWarning("データがありません");
+            return null;
         }
     }
 }
